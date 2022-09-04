550be41a75a8b1f5720e2b110b9fb2b294f94169
ShreySuri <shreysuri5@gmail.com>
2022-09-04 10:24:46 -0500

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -24,3 +24,44 @@ def graph(max_y, max_x):
     hori.up()
     vert.right(90)
     hori.right(90)
+
+    for i in range (1, 3):
+        vert.forward(375 * i)
+        hori.forward(375 * i)
+        vert.right(90)
+        hori.right(90)
+
+    hori.right(90)
+    vert.down()
+    hori.down()
+
+    vert_quotient = 750 / max_x
+    if int(vert_quotient) == vert_quotient:
+        vert_quotient = vert_quotient - 1
+    else:
+        toggle = True
+
+    hori_quotient = 1500 / max_y
+    if int(hori_quotient) == hori_quotient:
+        hori_quotient = hori_quotient - 1
+    else:
+        toggle = True
+
+    for i in range (0, max_x):
+        vert.forward(vert_quotient)
+        vert.right(90)
+        vert.forward(10)
+        vert.right(180)
+        vert.forward(10)
+        vert.right(90)
+
+    for i in range (0, max_y):
+        hori.forward(hori_quotient)
+        hori.left(90)
+        hori.forward(10)
+        hori.left(180)
+        hori.forward(10)
+        hori.left(90)
+
+
+x = graph(50, 25)   
