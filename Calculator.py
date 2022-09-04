a343390166ed6c622a8c7dd26b814cda46dfd41b
ShreySuri <shreysuri5@gmail.com>
2022-09-04 10:22:22 -0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+def isFloat(x):
+    accepted_list = ["."]
+    for i in range (0, 10):
+        i = str(i)
+        accepted_list.append(i)
+    period_count = 0
+    x = list(x)
+    length = len(x)
+    count = 0
+    for i in range (0, length):
+        for j in range (0, 11):
+            if x[i] == accepted_list[j]:
+                count = count + 1
+                if j == 0:
+                    period_count = period_count + 1
+                else:
+                    toggle = True
+            else:
+                toggle = False
+
+    if count == length and period_count < 2:
+        return(True)
+    else:
+        return(False)
